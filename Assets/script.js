9f6cc5c1dd257354a532b48f5636eb41b4fa47de
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-04 16:42:04 -0400

    HTML input numb for length added, script updated

--- Assets/script.js
+++ Assets/script.js
@@ -4,14 +4,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Write password to the #password input and set the length from the HTML
 
-  passwordText.value = password;
 
-}
+function pickOne() {
+  var length = document.getElementById("p-length").value;
+  var characters = "Yf#2G$t!Lq3PvZ8xUoVbI6sTc7XaQ9yWjMkE0nHm4eD5wRiKpR$Jm5DWLx14cwSQjkpYdAy6Nfz8FgiU0Xb9eKu2ZVsnGqahBOv7HtEP";
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var pickOne = Math.floor(Math.random() * characters.length);
+    password += characters[pickOne]
+  }
+  return password;
+};
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function writePassword()
+{
+    var passwordText = document.querySelector("#password");
+    var password = pickOne (9);
+    passwordText.value = password;
+}   )
